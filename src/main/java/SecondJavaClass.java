eb60f940ff0dc2cefec850359b891630de7f1270
fatihkoc1 <102413161+fatihkoc1@users.noreply.github.com>
2022-06-09 21:13:03 +0300

    changings in

--- src/main/java/SecondJavaClass.java
+++ src/main/java/SecondJavaClass.java
@@ -1,5 +1,7 @@
 public class SecondJavaClass {
     public static void main(String[] args) {
         System.out.println("Learning github");
+        System.out.println("Learning github12");
+        System.out.println("Learning github34");
     }
 }
